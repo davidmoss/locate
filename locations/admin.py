6b711f6fe242ab0d73511f1fef6dd3ab6a959905
David Moss <david@vowmade.com>
2019-11-21 08:19:38 +0000

    Start initial admin UI for importing file dumps

--- locations/admin.py
+++ locations/admin.py
@@ -1,8 +1,37 @@
+from django import forms
 from django.contrib import admin
 
 from .models import City, Country, Location, Region
+from .utils import get_geocountries, populate_country
 
-admin.site.register(Location)
-admin.site.register(Country)
+
+class LocationForm(forms.ModelForm):
+    file = forms.FileField(label='Import from file', required=False)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    form = LocationForm
+
+
+class CountriesForm(forms.ModelForm):
+    file = forms.FileField(label='Import from file', required=False)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        file = self.cleaned_data.get('file')
+        if file:
+            for geocountry in get_geocountries(file):
+                populate_country(geocountry)
+        if commit:
+            instance.save()
+        return instance
+
+
+class CountriesAdmin(admin.ModelAdmin):
+    form = CountriesForm
+
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Country, CountriesAdmin)
 admin.site.register(Region)
 admin.site.register(City)
